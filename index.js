d6a3998da333ef80d1e35321944ea06b8dfe1585
GMonLep <gmonterolepio@gmail.com>
2026-07-06 19:20:48 -0400

    feat: agregar documentación de la API con Swagger

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+//Documentacion con Swagger
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./config/swagger");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/mi_base_datos';
 
 app.use(express.json());
+
+//Swagger nuevamente
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+/**
+ * @openapi
+ * /:
+ *   get:
+ *     summary: Verifica que el microservicio está funcionando
+ *     description: Retorna el estado del servidor y un mensaje de confirmación.
+ *     tags:
+ *       - Estado
+ *     responses:
+ *       200:
+ *         description: Servidor funcionando correctamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               status: OK
+ *               message: Microservicio DevOps funcionando correctamente
+ *               integrante: Tu Nombre Completo
+ */
 
 // Ruta principal requerida para verificar que la app funciona
 app.get('/', (req, res) => {
